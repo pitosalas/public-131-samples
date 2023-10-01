639f411b2c7b1e2844d6d05c5171c3c3eec6088f
Pito Salas <pitosalas@users.noreply.github.com>
2023-09-30 20:59:21 -0400

    checkpoint

--- toy_os/toy_os/main.py
+++ toy_os/toy_os/main.py
@@ -17,15 +17,12 @@ class PCB:
         self.arrival_time = arrival_time
         self.burst_time = burst_time
         self.total_time = total_time
-        self.time = 0
+        self.run_time = 0
+        self.wall_time = 0
         self.next = None
 
-    def __str__(self):
-    
-
-        return "PID: " + str(self.pid) + " Arrival Time: " + str(self.arrival_time) + " Burst Time: " + str(self.burst_time) + " Total Time: " + str(self.total_time)
-
-
+    def update(self, time):
+        self.wall_time += 1
 
 class Queue:
     """
@@ -51,9 +48,11 @@ class Queue:
         if self.head == None:
             self.head = pcb
             self.tail = pcb
+            pcb.next = None
         else:
             self.tail.next = pcb
             self.tail = pcb
+
 
     def remove(self):
         if self.head == None:
@@ -61,6 +60,7 @@ class Queue:
         else:
             pcb = self.head
             self.head = self.head.next
+            pcb.next = None
             return pcb
 
     def print(self):
@@ -71,11 +71,12 @@ class Queue:
         table.add_column("Arrival", justify="right", style="green") 
         table.add_column("Burst", justify="right", style="green") 
         table.add_column("Total", justify="right", style="green") 
-        table.add_column("Current", justify="right", style="green") 
+        table.add_column("Current", justify="right", style="green")
+        table.add_column("Wall Time", justify="right", style="green") 
 
         pcb = self.head
         while pcb != None:
-            table.add_row(str(pcb.pid), str(pcb.arrival_time), str(pcb.burst_time), str(pcb.total_time), str(pcb.time))
+            table.add_row(str(pcb.pid), str(pcb.arrival_time), str(pcb.burst_time), str(pcb.total_time), str(pcb.run_time), str(pcb.wall_time))
             pcb = pcb.next
         console.print(table)
 
@@ -84,28 +85,62 @@ class Queue:
 # can maintain their state. Whenever a new object is created that needs to be aware of the time,
 # it registers itself with the clock object
 
-
 class Clock:
     def __init__(self):
+        self.watchers = []
         self.time = 0
 
     def increment(self):
         self.time += 1
+        for obj in self.watchers:
+            obj.update(self.time)
 
     def get_time(self):
         return self.time
 
     def register_object(self, obj):
-        obj.clock = self
+        self.watchers.append(obj)
 
-class Simulation:
+    def unregister_object(self, obj):
+        self.watchers.remove(obj)
+
+class Scheduler:
     def __init__(self):
-        print("contructor")
-        self.clock = Clock()
         self.new_queue = Queue("New Queue")
         self.ready_queue = Queue("Ready Queue")
         self.waiting_queue = Queue("Waiting Queue")
         self.terminated_queue = Queue("Terminated Queue")
+        self.running = Queue("Running")
+
+    def all_processes_done(self):
+        """
+        Returns True if all processes are done, False otherwise.
+        """
+        return self.running.head == None and self.new_queue.head == None and self.ready_queue.head == None and self.waiting_queue.head == None
+    
+    def update(self, time):
+        print("Scheduler update")
+        # while there are still pcbs on new queue, remove them from new queue and add them to ready queue
+        while self.new_queue.head != None:
+            pcb = self.new_queue.remove()
+            self.ready_queue.add(pcb)
+        # if there is a running process, check if it is done
+        if self.running.head is not None and self.running.head.run_time == self.running.head.total_time:
+            # if it is done, add it to the terminated queue and set running to None
+            self.terminated_queue.add(self.running.remove())
+        # if there is no running process, remove the first process from the ready queue and run it
+        if self.running.head == None and self.ready_queue.head != None:
+            self.running.add(self.ready_queue.remove())
+        # if there is a running process, increment its time
+        if self.running.head is not None:
+            self.running.head.run_time += 1
+
+
+class Simulation:
+    def __init__(self):
+        self.sched = Scheduler()
+        self.clock = Clock()
+        self.clock.register_object(self.sched)
 
     def print_status(self):
         """
@@ -114,16 +149,17 @@ class Simulation:
         console = Console()
         table = Table(show_header=True, header_style="bold magenta")
         table.title = "Operating System Status"
-        table.add_column("Variable", style="cyan")
+        table.add_column("", style="cyan")
         table.add_column("Value", justify="right", style="green") 
         table.add_row("Time", str(self.clock.get_time()))
         console.print(table)
-        self.new_queue.print()
-        self.ready_queue.print()
-        self.waiting_queue.print()
-        self.terminated_queue.print()
-        
+        self.sched.running.print()
+        self.sched.ready_queue.print()
+        self.sched.waiting_queue.print()
+        self.sched.terminated_queue.print()
+        self.sched.new_queue.print()
 
+        
     # Function to ead the json file
     def import_json_file(self, filename):
         with open(filename, 'r') as f:
@@ -144,14 +180,21 @@ class Simulation:
                 total_time = random.randint(
                     data['total_time']['from'], data['burst_time']['to'])
                 pcb = PCB(pid, arrival_time, burst_time, total_time)
-                self.new_queue.add(pcb)
+                self.sched.new_queue.add(pcb)
                 self.clock.register_object(pcb)
 
     def run(self):
         # import the json file
         self.import_json_file("processes.json")
-        self.print_status()
-
+        while (not self.sched.all_processes_done()):
+            response = input("[n(ext),s(top): ")
+            if response == 's':
+                break
+            elif response == 'n':
+                self.clock.increment()
+                self.print_status()
+            else:
+                print("Invalid response. Try again.")
 
 if __name__ == "__main__":
     s = Simulation()
