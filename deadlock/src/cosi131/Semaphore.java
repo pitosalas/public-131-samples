6217f3fded4a20933f1d05fd064461c9abd0f67b
Pito Salas <pitosalas@users.noreply.github.com>
2023-11-01 17:16:26 -0400

    checkpoint

--- deadlock/src/cosi131/Semaphore.java
+++ deadlock/src/cosi131/Semaphore.java
@@ -1,33 +1,26 @@
 package cosi131;
 
-
 public class Semaphore {
 	private volatile int permits;
 
 	Semaphore(int count) {
 		this.permits = count;
 	}
-	
+
 	public Semaphore() {
 		this(1);
 	}
-	
-	public String toString() {
-		return " "+this.permits;
-	}
 
-	
-	synchronized void  acquire() throws InterruptedException {
-		while(this.permits <= 0) {
+	synchronized void acquire() throws InterruptedException {
+		while (this.permits <= 0) {
 			wait();
 		}
 		this.permits--;
-		
+
 	}
-	
+
 	synchronized void release() {
 		this.permits++;
 		notify();
 	}
 }
-
