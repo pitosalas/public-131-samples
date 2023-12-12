cab24bd96cb0be36cdb5a784765d052f7fa061a1
Pito Salas <pitosalas@users.noreply.github.com>
2023-12-11 21:04:28 -0500

    checkpoint

--- memsim/src/lib/pagetables.py
+++ memsim/src/lib/pagetables.py
@@ -1,5 +1,52 @@
+from abc import ABC, abstractmethod
 from lib.utils import pretty_mem_str, collapse_contiguous_ranges
 
+class MemoryMapping(ABC):
+    def __init__(self, pagesize, capacity_bytes):
+        self.pagesize = pagesize
+        self.capacity_bytes = capacity_bytes
+
+    @abstractmethod
+    def access(self, logical_address):
+        pass
+
+"""
+We create this in its empty state. We allocate the outer page table with all None
+to indicate that there are no inner page tables yet. When we need to allocate a
+page, we allocate the inner page table, and then allocate the page within that
+table. The outer page table is indexed by the outer page number. The inner page
+table is indexed by the inner page number. The inner page number is the offset1
+from the logical address. The outer page number is the offset2 from the logical
+address.
+"""
+class TwoLevelPageTable(MemoryMapping):
+    def __init__(self, page_size: int, capcity_bytes: int):
+        super().__init__(page_size, capcity_bytes)
+        outer_page_count = capcity_bytes // page_size
+        self.table: list[None | list[int]] = [None] * outer_page_count
+        self.page_size = page_size
+
+    def access(self, logical_address: int) -> int | None:
+        outer_page_number, inner_page_number, page_offset = self.address_fields(logical_address)   
+        if self.table[outer_page_number] is None:
+            self.table[outer_page_number] = [None] * (self.page_size // 4)
+        inner_page_table = self.table[outer_page_number]
+        if inner_page_table[inner_page_number] is None:
+            inner_page_table[inner_page_number] = "allocated"
+    
+    def address_fields(self, logical_address: int) -> tuple[int, int, int]:
+        outer_page_number = logical_address // self.page_size
+        inner_page_number = (logical_address % self.page_size) // 4
+        page_offset = logical_address % 4
+        return outer_page_number, inner_page_number, page_offset
+    
+    def set_frame(self, page_number: int, frame_number: int):
+        self.table[page_number] = frame_number
+    
+    def __str__(self):
+        return f"TwoLevelPageTable:  {collapse_contiguous_ranges(self.table)}"
+
+   
 class Block:
     def __init__(self, start: int, size: int):
         self.physical_address = start
@@ -28,29 +75,6 @@ class PageTable:
     def __str__(self):
         return f"PageTable:  {collapse_contiguous_ranges(self.table)}"
     
-"""
-We create this in its empty state. We allocate the outer page table with all None
-to indicate that there are no inner page tables yet. When we need to allocate a
-page, we allocate the inner page table, and then allocate the page within that
-table. The outer page table is indexed by the outer page number. The inner page
-table is indexed by the inner page number. The inner page number is the offset1
-from the logical address. The outer page number is the offset2 from the logical
-address.
-"""
-class TwoLevelPageTable:
-    def __init__(self, pagesize: int, page_count: int):
-        self.table: list[None | int] = [None] * page_count
-        self.pagesize = pagesize
-        self.page_count = page_count
-
-    def get_frame(self, page_number: int) -> int | None:
-        return self.table[page_number]
-    
-    def set_frame(self, page_number: int, frame_number: int):
-        self.table[page_number] = frame_number
-    
-    def __str__(self):
-        return f"TwoLevelPageTable:  {collapse_contiguous_ranges(self.table)}"
 
 class PCB:
     def __init__(self, process: str, mapping: Block | PageTable):
@@ -59,3 +83,4 @@ class PCB:
 
     def __str__(self):
         return f"{self.process}  {self.mapping}"
+
